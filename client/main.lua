d182c0825fc87c956d55a0a2df05d4732415979a
Quin Schurman <schurmanq@gmail.com>
2022-02-01 22:34:06 -0500

    Convert indentation to spaces

--- client/main.lua
+++ client/main.lua
@@ -9,7 +9,7 @@ end)
 ]]
 
 RegisterNetEvent("919-speedlimit:client:ToggleSpeedLimit", function(toggle)
-	if toggle then
+    if toggle then
         SendNUIMessage({action = "show"})
         UIOpen = true
         SpeedLimitEnabled = true
@@ -23,17 +23,17 @@ end)
 Citizen.CreateThread(function()
     while true do
         Wait(1000)
-    	if IsPedInAnyVehicle(PlayerPedId()) then
-			if SpeedLimitEnabled and not UIOpen then
+        if IsPedInAnyVehicle(PlayerPedId()) then
+            if SpeedLimitEnabled and not UIOpen then
                 SendNUIMessage({action = "show"})
-    	    	UIOpen = true
+                UIOpen = true
             elseif SpeedLimitEnabled and UIOpen then
                 local speed = GetSpeedLimit()
                 if speed then
                     SendNUIMessage({action = "setlimit", speed = speed})
                 end
             end
-		else
+        else
             if SpeedLimitEnabled and UIOpen then
                 SendNUIMessage({action = "hide"})
                 UIOpen = false
@@ -43,6 +43,6 @@ Citizen.CreateThread(function()
 end)
 
 function GetSpeedLimit()
-	local coords = GetEntityCoords(PlayerPedId())
+    local coords = GetEntityCoords(PlayerPedId())
     return Config.SpeedLimits[GetStreetNameFromHashKey(GetStreetNameAtCoord(coords.x, coords.y, coords.z))]
 end
